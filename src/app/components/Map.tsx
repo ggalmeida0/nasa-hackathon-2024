81cad7d91cb6b9bbedc14341a56ee51b1d366fa8
Gabe <ggalmeida0@gmail.com>
2024-10-05 12:09:55 -0500

    basic map

---
+++ src/app/components/Map.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useEffect } from 'react'
+import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet'
+import { EditControl } from "react-leaflet-draw"
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-draw/dist/leaflet.draw.css'
+import L from 'leaflet'
+
+const Map = () => {
+  useEffect(() => {
+    // This is needed to fix Leaflet icons not displaying
+    delete (L.Icon.Default.prototype as any)._getIconUrl
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: '/leaflet/marker-icon-2x.png',
+      iconUrl: '/leaflet/marker-icon.png',
+      shadowUrl: '/leaflet/marker-shadow.png',
+    })
+  }, [])
+
+  const handleCreated = (e: any) => {
+    const { layer } = e
+    // Handle the created shape here
+    console.log(layer.toGeoJSON())
+  }
+
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <FeatureGroup>
+        <EditControl
+          position='topright'
+          onCreated={handleCreated}
+          draw={{
+            rectangle: true,
+            polygon: true,
+            circle: true,
+            circlemarker: false,
+            marker: true,
+            polyline: true,
+          }}
+        />
+      </FeatureGroup>
+    </MapContainer>
+  )
+}
+
+export default Map
