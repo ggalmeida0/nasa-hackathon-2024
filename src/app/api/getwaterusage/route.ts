376ac3f1ef7bc8f147e037ca94f16bb592787076
Aakash Haran <ash3498@tamu.edu>
2024-10-05 12:32:14 -0500

    added params to url

--- src/app/api/getwaterusage/route.ts
+++ src/app/api/getwaterusage/route.ts
@@ -1,28 +1,29 @@
 // app/api/hello/route.js
 export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+
+  const geometry : string = searchParams.get('geometry');
+  const startdate : string = searchParams.get('startdate');
+  const enddate : string = searchParams.get('enddate');
+  const interval : string = searchParams.get('interval');
+
+  const geometryET = JSON.parse(geometry).flat();
+
   const response = await fetch('https://openet-api.org/raster/timeseries/polygon', {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
       "Authorization": "pegB8xzf2qLW1l8L5lq8kqUow4GN4ujvMOtGKPZ2jLkWgk8KIpNfTzlLt8mP"
     },
+
     body: JSON.stringify({
       "date_range": [
-        "2020-01-01",
-        "2020-01-10"
+        startdate,
+        enddate
       ],
       "file_format": "JSON",
-      "geometry": [
-        -119.7937,
-        35.58995,
-        -119.7937,
-        35.53326,
-        -119.71268,
-        35.53326,
-        -119.71268,
-        35.58995
-      ],
-      "interval": "daily",
+      "geometry": geometryET,
+      "interval": interval,
       "model": "SSEBop",
       "reducer": "mean",
       "reference_et": "gridMET",
@@ -36,5 +37,9 @@ export async function GET(request) {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
+  // return new Response(JSON.stringify({ et_data: "hello" }), {
+  //   status: 200,
+  //   headers: { 'Content-Type': 'application/json' },
+  // });
 }
 
