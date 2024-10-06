7e1ab37c5a4c03ab453de6bd83093c60c8e29a20
Arun <arunguruswamy22@gmail.com>
2024-10-06 14:16:57 -0400

    bot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import dynamic from 'next/dynamic'
+import Chat from '@/app/components/chatBot'
 
 const MapWithNoSSR = dynamic(() => import('@/app/components/Map'), {
   ssr: false,
 })
 
+
 export default function Home() {
   return (
     <main>
       <h1>Map Drawing App</h1>
-      <MapWithNoSSR />
+      {/* <MapWithNoSSR /> */}
+      <Chat />
     </main>
   )
 }
